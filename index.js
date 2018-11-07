6092f0b9d527551869fa7b9540fbb90d85d84ff2
sunrise <sunrise1115@outlook.com>
2018-11-08 05:07:52 +0800

    added find module

---
+++ index.js
@@ -0,0 +1,51 @@
+var MongoClient = require('mongodb').MongoClient;
+var async = require('async');
+var findModel = require('./find');
+
+const url = `mongodb://127.0.0.1:27017/`;
+const input = (payload) => {
+  return new Promise((resolve, reject) => {
+    if (!payload || typeof payload.database == 'undefined')
+      reject({status: 'failed', msg: "database is undefined"});
+    if (!payload || typeof payload.type == 'undefined' || payload.type == '')
+      reject({status: 'failed', msg: "Type is undefined!"});
+    async.waterfall([
+      function(callback) {
+        MongoClient.connect(url, (err, connection) => {
+          if (!connection) return callback(null, true, {msg: 'connection error'});
+          return callback(null, false, {db: connection.db(payload.database)});
+        });
+      },
+      function(error, data, callback) {
+        if (error) return callback(error, data);
+        // filtering
+        var db = data.db;
+        var cursor = db.collection(payload.collection);
+        
+        switch (payload.type) {
+          case 'update':
+            break;
+          case 'save':
+            break;
+          case 'delete':
+            break;
+          default:
+            findModel.exec(cursor, payload).then(data => {
+              callback(null, data);
+            })
+            .catch(err => {
+              callback(true, {msg: 'fetching data error'});
+            });
+            break;
+        }
+      }
+    ], function(error, result) {
+      if (error) reject({status: 'failed', msg: result.msg});
+      else resolve({status: 'success', data: result});
+    });
+  });
+}
+
+module.exports = {
+  input: input
+}
--- test/index.js
+++ test/index.js
@@ -1,6 +1,5 @@
 const j2m = require('../index.js');
-const test1 = require('test1.json');
-
+const test1 = require('./test1.json');
 // this is test one:
 // we try to query the __auth__/Profile and console.log the results.
 // pls note should implement promisses as the old version.
@@ -8,7 +7,10 @@ const test1 = require('test1.json');
 // We try to make this: 
 j2m
   .input(test1)
-  .then(console.log);
+  .then(console.log)
+  .then(() => {
+    process.exit(1);
+  });
   
 // into this:
 // const { query, filter, sort, skip, limit }  = json1
