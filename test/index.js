07f4e4175ec187d75500bbdce5f05a07f87d51dd
Edson Tégila <etegila@gmail.com>
2018-11-07 16:16:50 -0200

    Update index.js

--- test/index.js
+++ test/index.js
@@ -4,7 +4,13 @@ const test1 = require('test1.json');
 // this is test one:
 // we try to query the __auth__/Profile and console.log the results.
 // pls note should implement promisses as the old version.
+
+// We try to make this: 
 j2m
   .input(test1)
   .then(console.log);
   
+// into this:
+// const { query, filter, sort, skip, limit }  = json1
+// db.Profiles.find(query, filter).sort(sort).skip(skip).limit(limit);
+// and send the answer back
