bab1816d6ca007a68b1845acf70dcda4f26ae5d3
Edson Tegila Goncalves <etegila@gmail.com>
2018-11-09 18:55:11 -0200

    simplifying test case

--- test/index.js
+++ test/index.js
@@ -5,97 +5,27 @@ const updateJson = require('./task.updateOne.json');
 const deleteJson = require('./task.remove.json');
 var async = require('async');
 
-// updateOne completed from false to true
-// remove all completed true 
-
-// this is test one:
-// we try to query the __auth__/Profile and console.log the results.
-// pls note should implement promisses as the old version.
-
-// We try to make this: 
-async.waterfall([
-  (callback) => {
-    console.log('database initial status');
-    j2m.input(findJson)
-      .then(console.log)
-      .then(() => {
-        callback(null, false);
-      })
-      .catch(err => {
-        console.log(err);
-        callback(null, true);
-      });
-  }, // insert testing
-  (err, callback) => {
-    if (err) return callback(null, true);
-    j2m.input(insertJson)
-      .then(res => {
-        console.log(res);
-        j2m.input(findJson)
-          .then(data=> {
-            console.log(data);
-            callback(null, false);
-          })
-          .catch(err => {
-            console.log(err);
-            callback(null, true);
-          });
-      })
-      .catch(err => {
-        console.log(err);
-        callback(null, true);
-      });
-  }, // update testing
-  (err, callback) => {
-    if (err) return callback(null, true);
-    j2m.input(updateJson)
-      .then(res => {
-        console.log(res);
-        j2m.input({
-            "type": "find",
-            "database": "app",
-            "collection": "Todos",
-            "query": {}
-          })
-          .then(data => {
-            console.log(data);
-            callback(null, false);
-          })
-          .catch(err => {
-            console.log(err);
-            callback(null, true);
-          });
-      })
-      .catch(err => {
-        console.log(err);
-        callback(null, true);
-      });
-  }, // delete testing
-  (err, callback) => {
-    if (err) return callback();
-    j2m.input(deleteJson)
-      .then(res => {
-        console.log(res);
-        j2m.input({
-            "type": "find",
-            "database": "app",
-            "collection": "Todos",
-            "query": {}
-          })
-          .then(data => {
-            console.log(data);
-            callback();
-          })
-          .catch(err => {
-            console.log(err);
-            callback();
-          });
-      })
-      .catch(err => {
-        console.log(err);
-        callback();
-      });
+// https://caolan.github.io/async/docs.html#queue
+const q = async.queue((raw_input) => {
+  return (callback) => {
+    j2m.input(raw_input)
+    .then(console.log)
+    .then(() => {
+      callback(null, false);
+    })
+    .catch(err => {
+      console.log(err);
+      callback(err, null);
+    });
   }
-], () => {
+}, 1);
+// 1 each time (Series)
+
+q.drain = function() {
+  console.log('all items have been processed');
   process.exit(1);
+};
+
+q.push([insertJson, findJson, updateJson, deleteJson], () => {
+  console.log('database initial status');
 });
