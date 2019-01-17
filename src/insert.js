1c373621bd365b4de7c2394d93a4d80b10478c92
Frederico F. de Oliveira <fredf_oliveira@ufu.br>
2019-01-17 16:34:22 -0200

    Finish implementing insertMany

--- src/insert.js
+++ src/insert.js
@@ -1,8 +1,15 @@
 module.exports = {
   input: (db, payload, resolve, reject) => {
-    db.insertOne(payload.data, (err, result) => {
-      if (err) return reject(err);
-      resolve(result);
-    });
+    if (payload.type == "insertOne") {
+      db.insertOne(payload.data, (err, result) => {
+        if (err) return reject(err);
+        resolve(result);
+      });
+    } else if (payload.type == "insertMany") {
+      db.insertMany(payload.data, (err, result) => {
+        if (err) return reject(err);
+        resolve(result);
+      });
+    }
   }
 }
