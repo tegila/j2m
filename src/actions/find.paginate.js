fcbb8d8bc6d6cb7669e460a973cdb8af6cbbe5bc
Frederico F. de Oliveira <fredf_oliveira@ufu.br>
2019-01-25 15:38:49 -0200

    Fix limit default value in find.paginate.js

--- src/actions/find.paginate.js
+++ src/actions/find.paginate.js
@@ -7,7 +7,7 @@ module.exports = runners => {
     input: (db, payload, resolve, reject) => {
       db.find(payload.filter)
         .skip(payload.skip || 0)
-        .limit(payload.limit || null)
+        .limit(payload.limit || 0)
         .toArray((err, docs) => {
           if (err) return reject(err);
           resolve(docs);
